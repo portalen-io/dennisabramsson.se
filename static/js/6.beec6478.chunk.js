09fd04a3fc55ad385f578e0edcf8770cf0c49e54
Gustav Eklund <gustavpaeklund@gmail.com>
2021-01-13 15:10:52 +0100

    Updates

--- static/js/6.beec6478.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdennisabramsson.se"]=this["webpackJsonpdennisabramsson.se"]||[]).push([[6],{43:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return r}));var s=t(0),a=t.n(s);function r(){return a.a.createElement("div",{id:"NotFound"},a.a.createElement("h1",null,"NotFound"),a.a.createElement("p",null,"404"))}}}]);
-//# sourceMappingURL=6.beec6478.chunk.js.map
