01b5f0f42192875cb66bdb5b448c992b9922e769
Gustav Eklund <gustavpaeklund@gmail.com>
2021-01-14 12:13:00 +0100

    Add XXX to title

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const NotFound = lazy(() => import('./routes/NotFound'));
 
 export const DataContext: React.Context<IData> = React.createContext({
     welcome: {
-        title: 'Adaptable\nCooperative\nCurious',
+        title: 'AdaptableXXX\nCooperative\nCurious',
         frontImage: {
             name: 'projects/me',
             type: 'png',
